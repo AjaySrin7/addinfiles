eab3095cc3b230abb280888a285d47b3bb4ff385
AjaySrin7 <54893164+AjaySrin7@users.noreply.github.com>
2022-04-18 19:40:19 -0400

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,49 @@
+var data = {
+    labels: ['Exception 1 ', 'Exception 2'],
+    datasets: [{
+        fill: true,
+        backgroundColor: ['red', 'green'],
+        data: [5, 95],
+        // Notice the borderColor
+        borderColor: ['black', 'black'],
+        borderWidth: [2, 2]
+    }]
+}
+var options = {
+    title: {
+        display: true,
+        text: 'Exceptions By Type'
+
+    }
+
+}
+
+function myFunction() {
+    console.log('Page is loaded')
+    var head = document.getElementsByTagName('head')[0]
+    var script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.src =
+        'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js'
+    script.id = 'chartLibrary'
+    head.appendChild(script)
+
+    var canvas = document.getElementById('chartCanvas')
+    var ctx = canvas.getContext('2d')
+
+    var myChartLib = document.querySelector('#chartLibrary')
+    myChartLib.addEventListener('load', function() {
+        // Chart declaration:
+        var myPieChart = new Chart(ctx, {
+            type: 'pie',
+            data: data,
+            options: options
+        })
+    })
+
+
+
+    // Notice the rotation from the documentation.
+
+
+}
