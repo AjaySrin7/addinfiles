f254af21b2f279b7f491390e569e92edfc1f1781
AjaySrin7 <54893164+AjaySrin7@users.noreply.github.com>
2022-04-19 00:57:28 -0400

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,36 +1,31 @@
-var _fromDate = document.querySelector("#startDate").value; //from Date picker
-var _toDate = document.querySelector("#endDate").value;  //from Date picker  
+// Chart parameters
 
-// To process submit button click
+var idlingCount = 0
+var speedingCount =0
 
-function GenerateChart(api,_fromDate,_toDate){
-GetExceptions(api,_fromDate,_toDate,"bB","RuleIdlingId")
-.then(res=>{
-    console.log(res)
-    GetExceptions(api,_fromDate,_toDate,"bB","RulePostedSpeedingId")
-    .then(res=>{
-        console.log(res)
-        LoadChart()
-    })
-})
-     
-}
-
-
-// To populate canvas with chart 
-function LoadChart() {
-    console.log('Page is loaded')
-    var chartData = {
-    labels: ['Idling', 'Speeding'],
+var chartData = {
+    labels: ['Exception 1 ', 'Exception 2'],
     datasets: [{
         fill: true,
         backgroundColor: ['red', 'green'],
-        data: [idlingCount, speedingCount],       
+        data: [5, 95],
+        // Notice the borderColor
         borderColor: ['black', 'black'],
         borderWidth: [2, 2]
     }]
 }
-    console.log('cd',chartData)
+var options = {
+    title: {
+        display: true,
+        text: 'Exceptions By Type'
+
+    }
+
+}
+
+// To populate canvas with chart 
+function LoadChart() {
+    console.log('Page is loaded')
     var head = document.getElementsByTagName('head')[0]
     var script = document.createElement('script')
     script.type = 'text/javascript'
@@ -55,4 +50,41 @@ function LoadChart() {
 
 }
 
+// To fetch exception events
+async function GetExceptions(_fromDate,_toDate,_deviceId,_ruleId){
+    
 
+return new Promise((resolve,reject)=>{
+    
+api.call("Get", {
+    "typeName": "ExceptionEvent",
+    "search":{
+        fromDate:_fromDate,
+        toDate:_toDate,
+        deviceSearch:{
+            id:_deviceId
+        },
+        
+            ruleSearch:{
+                id:_ruleId
+            }
+         
+    },
+}, function(result) {
+    if(_ruleId==="RuleIdlingId"){
+        idlingCount=result.length
+        resolve(idlingCount)
+    }
+    else{
+        if(_ruleId==="RulePostedSpeedingId"){
+            speedingCount=result.length
+            resolve(speedingCount)
+        }
+    }
+    return result.length
+}, function(e) {
+    resolve(0)
+});
+})
+
+};
